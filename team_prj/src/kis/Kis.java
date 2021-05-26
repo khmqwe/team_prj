232689c1b662704f4b33a538d4ab2138d0b9e871
user <user@DESKTOP-COJ9858>
2021-05-26 10:27:38 +0900

    add kis

---
+++ team_prj/src/kis/Kis.java
@@ -0,0 +1,16 @@
+package kis;
+
+public class Kis {
+	
+	
+	public Kis() {
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5
+					; j++) {
+				System.out.print("kis");
+			}
+			System.out.println();
+		}
+	}
+
+}
